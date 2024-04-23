293a69e05e507b1d4c35a850d6d4f138e8697049
Álvaro Troya <alvaro.troya@googlemail.com>
2024-04-23 21:40:14 +0200

    Read a video from FS IO and display it's frames.

--- sittube/videostreamer/videostreamer.py
+++ sittube/videostreamer/videostreamer.py
@@ -1,5 +1,21 @@
+"""
+Ideally what we want here is a video/buffer like interface:
+VideoStreamer.read() -> returns a frame
+VideoStreamer.seek() -> seeks to a frame
+We should probably use a context manager for opening and closing the video file
+
+Let's start with reading a file from disk
+"""
+import cv2
+
+
 def main():
-    print("Hello, world!")
+    cap = cv2.VideoCapture('/home/alvaro/repos/mine/sittube/resources/countdown.mp4')
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        cv2.imshow('frame', frame)
 
 
 if __name__ == '__main__':
