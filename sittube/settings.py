db83fe32730c46f7a602ef08669e63f93888a9f1
Álvaro Troya <alvaro.troya@googlemail.com>
2024-04-24 11:04:10 +0200

    Try to create out directory automatically.

--- sittube/settings.py
+++ sittube/settings.py
@@ -5,20 +5,25 @@ from pathlib import Path
 
 class Settings(BaseSettings):
     video_source: Path
-    frame_buffer_size: int = 10
+    frame_buffer_size: int = 50
     target_location: Path
 
     class Config:
-        env_file = ".env"
-
-    @validator("target_location")
-    def validate_target_location(cls, value):
-        if not value.exists():
-            raise FileNotFoundError("Target location does not exist.")
-        return value
+        env_file = "../.env"
 
     @validator("video_source")
     def validate_video_source(cls, value):
         if not value.exists():
             raise FileNotFoundError("Video source does not exist.")
+        return value
+
+    @validator("target_location")
+    def validate_target_location(cls, value):
+        if not value.exists():
+            try:
+                value.mkdir(parents=True)
+            except Exception as e:
+                print("Error creating target location")
+                print(f"Consider creating the directory manually: {value}")
+                print(f"Error: {e}")
         return value
