f5c39cbdfa7e9164199f77c51ea850766255bcf2
Álvaro Troya <alvaro.troya@googlemail.com>
2024-04-24 03:56:55 +0200

    Use a thread for consuming the video stream.

--- sittube/videostreamer/videostreamer.py
+++ sittube/videostreamer/videostreamer.py
@@ -2,7 +2,7 @@
 Ideally what we want here is a video/buffer like interface:
 VideoStreamer.read() -> returns a frame
 VideoStreamer.seek() -> seeks to a frame
-We should probably use a context manager for opening and closing the video file
+We should probably use a context manager for opening and closing the video stream
 
 Let's start with reading a file from disk.
 Now, let's store the frames in a buffer and implement the read method.
@@ -10,7 +10,8 @@ Now, let's store the frames in a buffer and implement the read method.
 
 import datetime
 import functools
-import time
+import threading
+import time  # noqa: F401
 
 import cv2
 
@@ -21,7 +22,7 @@ class VideoBuffer:
         self,
         filename: str,
         buffer_size: int = 50,
-        buffer_step: int = 30,
+        buffer_step: int = 2,
     ):
         self.cap = None
         self.buffer = []  # stores the frames of the video
@@ -29,44 +30,55 @@ class VideoBuffer:
         self.filename = filename
         self.buffer_size = buffer_size  # number of frames to store in the buffer
         self.buffer_step = buffer_step  # number of frames to skip between frames
+        self.lock = threading.Lock()  # lock the buffer for thread safety
+        self.running = False
         self.start_ts = datetime.datetime.utcnow()
 
     def __enter__(self):
         self.cap = cv2.VideoCapture(self.filename)
-        self._fill_buffer()
+        self.running = True
+        # consume the video stream in a separate thread
+        thread = threading.Thread(target=self._fill_buffer, daemon=True)
+        thread.start()
+        time.sleep(0.1)  # allow the buffer to fill up
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        self.running = False
         self.cap.release()
 
     def _fill_buffer(self):
         step = 0
-        while self.cap.isOpened():
+        while self.running and self.cap.isOpened():
             ret, frame = self.cap.read()
             if not ret:
+                self.running = False
                 break
 
-            if len(self.buffer) > self.buffer_size:
-                self._pop()
+            with self.lock:
+                if step % self.buffer_step == 0:
+                    print("writing frame to buffer")
+                    self.buffer.append(frame)
+                    self.frame_index.append(step)
 
-            if step % self.buffer_step == 0:
-                self.buffer.append(frame)
-                self.frame_index.append(step)
+                    if len(self.buffer) > self.buffer_size:
+                        self._pop()
 
-            # print(self)
-            # time.sleep(0.1)
+            print(self)
+            time.sleep(0.1)
             step += 1
 
     def __repr__(self):
-        return (
-            f"VideoBuffer: "
-            f"Total number of frames: {int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))}, "
-            f"Buffer step: {self.buffer_step}, "
-            f"Frames in buffer: {len(self.buffer)}, "
-            f"Current frame: {self.frame_index[-1] if self.frame_index else -1}, "
-            f"Original FPS: {self.cap.get(cv2.CAP_PROP_FPS)}, "
-            f"Buffered FPS: {self.fps:.2f})"
-        )
+        with self.lock:
+            return (
+                f"VideoBuffer: "
+                f"Total number of frames: {int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))}, "
+                f"Buffer step: {self.buffer_step}, "
+                f"Frames in buffer: {len(self.buffer)}, "
+                f"Current frame: {self.frame_index[-1] if self.frame_index else 'N/A'}, "
+                f"Original FPS: {self.cap.get(cv2.CAP_PROP_FPS)}, "
+                f"Buffered FPS: {self.fps:.2f})"
+            )
 
     def __len__(self):
         return len(self.buffer)
@@ -84,21 +96,26 @@ class VideoBuffer:
         return frame_index
 
     def read(self):
-        if self.buffer:
-            _, frame = self._pop()
-            return True, frame
-        else:
-            return False, None
+        with self.lock:
+            if self.buffer:
+                _, frame = self._pop()
+                return True, frame
+            else:
+                return False, None
 
     def _pop(self):
         return self.frame_index.pop(0), self.buffer.pop(0)
 
     def show(self):
-        for frame in self.buffer:
-            cv2.imshow("frame", frame)
+        while self.running:
+            if not self.buffer:
+                continue
 
-            # interrupt the display if q is pressed
-            if cv2.waitKey(100) & 0xFF == ord("q"):
+            with self.lock:
+                current_frame = self.buffer[-1]
+
+            cv2.imshow("frame", current_frame)
+            if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
 
             # print(self)
@@ -126,36 +143,26 @@ class VideoBuffer:
         right_index = min(len(self), curr_frame + num_frames_right)
 
         # TODO: this method should actually return a new VideoBuffer instance, skipping now for simplicity
-        return self.buffer[left_index:right_index]
+        with self.lock:
+            return self.buffer[left_index:right_index]
 
 
 def main():
     start = datetime.datetime.utcnow()
     VIDEO_PATH = "/home/alvaro/repos/mine/sittube/resources/countdown.mp4"
     with VideoBuffer(VIDEO_PATH) as video:
-        # video.show()
-        # while True:
-        #     ret, frame = video.read()
-        #
-        #     if not ret:
-        #         print("End of video. Exiting...")
-        #         break
-        #
-        #     # print(video)
-        #     cv2.imshow("frame", frame)
-        #     if cv2.waitKey(100) & 0xFF == ord("q"):
-        #         break
         # for i in range(len(video)):
         #     # print(i)
         #     cv2.imshow("frame", video[i])
         #     if cv2.waitKey(100) & 0xFF == ord("q"):
         #         break
 
-        time.sleep(5)
+        time.sleep(10)
         now = datetime.datetime.utcnow()
-        for frame in video.slice_at_timestamp(now, num_frames_right=200):
+        for i, frame in enumerate(video.slice_at_timestamp(now, num_frames_right=50)):
             # print(i)
-            cv2.imshow("frame", frame)
+            print(f"showing frame {i}")
+            cv2.imshow("foo", frame)
             if cv2.waitKey(100) & 0xFF == ord("q"):
                 break
 
